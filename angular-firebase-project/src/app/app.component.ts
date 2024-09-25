70074dd8cbc2cc7bd483256763128187cfa2f8f2
vaskata02 <vas.kata02@gmail.com>
2024-09-25 21:12:16 +0200

    firebase beállítás, tesztelés

--- angular-firebase-project/src/app/app.component.ts
+++ angular-firebase-project/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { collection, collectionData, Firestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { TestModel } from "./models/test.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'angular-firebase-project';
+  title = "angular-firebase-project";
+  tests$: Observable<TestModel[]>;
+  private readonly testCollectionRef = collection(this.firestore, "test");
+
+  constructor(private firestore: Firestore) {
+    this.tests$ = this.getTests();
+  }
+
+  getTests(): Observable<TestModel[]> {
+    return collectionData(this.testCollectionRef, {
+      idField: "id",
+    }) as Observable<TestModel[]>;
+  }
 }
--- angular-firebase-project/src/app/app.module.ts
+++ angular-firebase-project/src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyAedo5PI4iVpLdpAAyV2OTSjhgl7pyVoIY",
+        authDomain: "angular-firebase-project-e901f.firebaseapp.com",
+        projectId: "angular-firebase-project-e901f",
+        storageBucket: "angular-firebase-project-e901f.appspot.com",
+        messagingSenderId: "929539061263",
+        appId: "1:929539061263:web:06c3853913c27f235f957a",
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
