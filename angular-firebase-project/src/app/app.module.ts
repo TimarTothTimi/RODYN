78f19b31cc62b64683398f726f9d4df9efadb245
Timártót <toth.timea8@gmail.com>
2024-10-07 14:02:32 +0200

    angular -bravo - header

--- angular-firebase-project/src/app/app.module.ts
+++ angular-firebase-project/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
 import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideFirebaseApp(() =>
