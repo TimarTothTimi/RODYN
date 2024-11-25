22311e5a643644e1a867fb1d52dea94e7cacadf4
vaskata02 <vas.kata02@gmail.com>
2024-11-25 20:55:11 +0100

    product card, product page javítása

--- angular-firebase-project/src/app/components/product-page/product-page.component.ts
+++ angular-firebase-project/src/app/components/product-page/product-page.component.ts
@@ -15,6 +15,7 @@ export class ProductPageComponent implements OnInit, OnDestroy {
   public userEmail$?: Observable<string | null>;
   public product?: Product;
   destroy$: Subject<void> = new Subject<void>();
+  relatedProducts: Product[] = [];
 
   constructor(
     private authService: AuthService,
@@ -27,17 +28,30 @@ export class ProductPageComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    const productId = this.route.snapshot.paramMap.get("id");
-    const productCategory = this.route.snapshot.paramMap.get("category");
-    if (productCategory && productId) {
-      this.productService
-        .getProduct(productCategory, productId)
-        .pipe(takeUntil(this.destroy$))
-        .subscribe((product) => {
-          this.product = product;
-        });
-    }
+    this.route.paramMap.pipe(takeUntil(this.destroy$)).subscribe((paramMap) => {
+      const productId = paramMap.get("id");
+      const productCategory = paramMap.get("category");
+      if (productCategory && productId) {
+        this.productService
+          .getProduct(productCategory, productId)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe((product) => {
+            this.product = product;
+          });
+      }
+      if (productCategory) {
+        this.productService
+          .getProducts(productCategory)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe((products) => {
+            this.relatedProducts = products
+              .filter((product) => product.id !== productId)
+              .slice(0, 3);
+          });
+      }
+    });
   }
+
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
