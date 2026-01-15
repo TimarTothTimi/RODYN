2021043cb1cc5d6ec0e06c412252379fd97b4780
Timártót <toth.timea8@gmail.com>
2026-01-15 22:35:43 +0100

    header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
-  standalone: true,
-  imports: [CommonModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
+  showText = false;
+
+  ngAfterViewInit(): void {
+    const video = this.heroVideo.nativeElement;
+
+    // Ha a videó befejeződik, jelenjen meg a szöveg
+    video.addEventListener('ended', () => {
+      this.showText = true;
+      // fix utolsó képkocka (marad a videóból)
+      video.pause();
+    });
+  }
+}
