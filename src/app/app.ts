5fb5d6bc2d175d0e74c6b48dd50ad123737379d3
Timártót <toth.timea8@gmail.com>
2026-01-16 11:16:09 +0100

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 // src/app/app.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
